bb207eb3afa5a82edeb3858583136a8805d490cc
nknrw <nknrw@yandex.ru>
2023-08-19 23:09:10 +0300

    fix: styles

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
-<template>
+<template xmlns='http://www.w3.org/1999/html'>
   <div class='main'>
     <h1 class='heading'>Список продуктов</h1>
+    <div class='add-container'>
+      <input v-model='newProduct' @keyup.enter='addProduct' placeholder='Введите название продукта' class='add-input'>
+    </div>
     <ul class='list'>
-      <li class='add-container list-item'>
-        <input v-model='newProduct' @keyup.enter='addProduct' placeholder='Введите название продукта' class='add-input'>
-      </li>
       <li v-for='(product, index) in products'
           :key='index'
           class='list-item'
           @click='crossOut(product)'>
-        <span :class="{ 'crossed-out': product.crossed }">{{ product.name }}</span>
-        <button @click.stop='removeProduct(index)'>Удалить</button>
+        <span :class="{ 'crossed-out': product.crossed }" class='list-item_text'>{{ product.name }}</span>
       </li>
     </ul>
     <button class='clear-button' @click.stop='clearList'>Очистить</button>
@@ -28,7 +27,7 @@ export default {
   methods: {
     addProduct() {
       if (this.newProduct.trim() !== '') {
-        this.products.push({name: this.newProduct, crossed: false});
+        this.products.unshift({name: this.newProduct, crossed: false});
         this.newProduct = '';
         this.saveProducts();
       }
@@ -36,10 +35,6 @@ export default {
     crossOut(product) {
       product.crossed = !product.crossed;
       this.products.sort((a, b) => (a.crossed === b.crossed) ? 0 : a.crossed ? 1 : -1);
-      this.saveProducts();
-    },
-    removeProduct(index) {
-      this.products.splice(index, 1);
       this.saveProducts();
     },
     saveProducts() {
@@ -55,7 +50,7 @@ export default {
 
 <style>
 
-#app {
+html {
   margin: 0;
   padding: 0;
 }
@@ -68,25 +63,24 @@ body {
 }
 
 .main {
-  margin: 10px auto;
+  margin: 0 auto;
   padding: 0;
   display: flex;
   flex-direction: column;
   width: calc(100% - 20px);
   max-width: 400px;
-  max-height: 100dvh;
 }
 
 .heading {
   padding: 0;
-  margin: 0 0 20px 0;
+  margin: 20px 0;
   text-align: center;
   font-size: 32px;
 }
 
 .crossed-out {
   text-decoration: line-through;
-  color: #ccc;
+  color: #808080;
 }
 
 .list {
@@ -103,27 +97,23 @@ body {
   align-items: center;
   margin: 0;
   font-size: 24px;
-  justify-content: space-between;
-  border-bottom: 1px #ccc solid;
+  width: 100%;
+  border-bottom: 1px #808080 solid;
   box-sizing: border-box;
   line-height: 1;
   padding: 10px 0;
+  cursor: pointer;
+}
+
+.list-item_text {
+  width: 100%;
+  inline-size: 100%;
+  overflow-wrap: break-word;
+  hyphens: auto;
 }
 
 .list-item:last-of-type {
   border: none;
-}
-
-.list-item button {
-  display: none;
-  border: none;
-  background-color: unset;
-  color: #ff0000;
-  box-sizing: border-box;
-}
-
-.list-item:hover button {
-  display: flex;
 }
 
 .add-container {
@@ -133,20 +123,14 @@ body {
 .add-input {
   border: none;
   font-size: 24px;
-  padding: 0;
+  padding: 10px 0;
   width: 100%;
   text-overflow: ellipsis;
+  border-bottom: 1px #808080 solid;
 }
 
 .add-input:focus {
-  border: none;
   outline: none;
-}
-
-.add-button {
-  border: none;
-  background: none;
-  font-size: 16px;
 }
 
 .clear-button {
@@ -154,12 +138,8 @@ body {
   border: none;
   padding: 0;
   width: fit-content;
-  margin: 0 auto;
+  margin: 20px auto;
   cursor: pointer;
-}
-
-.clear-button:hover {
-  border-bottom: 1px #000 solid;
 }
 
 </style>
